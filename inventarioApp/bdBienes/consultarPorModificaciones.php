151b2410dfc0ae6778068e0b0014007ca5cd7eea
bhagam19 <bhagam19@gmail.com>
2022-06-21 16:04:41 -0500

    retoma

--- inventarioApp/bdBienes/consultarPorModificaciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('../conexion/datosConexion.php');
-
-	$consulta=mysqli_query($conexion,"SELECT DISTINCT codBien FROM modificacionesBienes ORDER BY codBien ASC");
-	$rows=mysqli_num_rows($consulta);
-
-	echo $rows;
-
-?>
