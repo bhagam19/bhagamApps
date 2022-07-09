0da5d60bed82438c988fc163155df9a0af0b0aa5
Bhagam19 <bhagam19@gmail.com>
2022-07-08 21:13:23 -0500

    Se agregó informe PAE

--- EvPAv/adm/03-cnt/00-cargarCsvAnalisisPreguntas.php
+++ EvPAv/adm/03-cnt/00-cargarCsvAnalisisPreguntas.php
@@ -45,61 +45,63 @@
     $cnt=1;
     $file = fopen($nombreArchivo,"r");
     while(($col=fgetcsv($file,10000,",")) !== FALSE){	
-		$idPregunta = $col[0];
-		$clave = $col[1];
-        $grado = $col[2];
-        $grupo = $col[3];
-        $instrumento = $col[4];
-        $cuadernillo = $col[5];
-        $componente = $col[6];
-        $competencia = $col[7];
-        $afirmacion = $col[8];
-        $evidencia = $col[9];
-        $nivelDificultad = $col[10];	
-        $respCorrectas = $col[11];	
-        $omisiones = $col[12];
-        $opcA = $col[13];
-        $opcB = $col[14];
-        $opcC = $col[15];
-        $opcD = 0;
-        $opcE = 0;
-        $opcF = 0;
-        $opcG = 0;
-        $opcH = 0;
-		echo '<td>'.$cnt.'</td>';
-		echo '<td>'.$idPregunta.'</td>';
-		echo '<td>'.$clave.'</td>';
-        echo '<td>'.$grado.'</td>';
-        echo '<td>'.$grupo.'</td>';
-        echo '<td>'.$instrumento.'</td>';
-        echo '<td>'.$cuadernillo.'</td>';
-        echo '<td>'.$componente.'</td>';
-        echo '<td>'.$afirmacion.'</td>';
-        echo '<td>'.$competencia.'</td>';
-        echo '<td>'.$evidencia.'</td>';
-        echo '<td>'.$nivelDificultad.'</td>';
-        echo '<td>'.$respCorrectas.'</td>';
-        echo '<td>'.$omisiones.'</td>';
-        echo '<td>'.$opcA.'</td>';
-        echo '<td>'.$opcB.'</td>';
-        echo '<td>'.$opcC.'</td>';
-        echo '<td>'.$opcD.'</td>';
-        echo '<td>'.$opcE.'</td>';
-        echo '<td>'.$opcF.'</td>';
-        echo '<td>'.$opcG.'</td>';
-        echo '<td>'.$opcH.'</td>';
-		echo '</tr>';			
-		$sql='
-            INSERT INTO analisisPreguntas(idPregunta,clave,grado,grupo,instrumento,cuadernillo,componente,afirmacion,competencia,evidencia,nivelDificultad,respCorrectas,omisiones,opcA,opcB,opcC,opcD,opcE,opcF,opcG,opcH) 
-            VALUES ("'.$idPregunta.'","'.$clave.'","'.$grado.'","'.$grupo.'","'.$instrumento.'",'.$cuadernillo.',"'.$componente.'","'.$afirmacion.'","'.$competencia.'",
-            "'.$evidencia.'","'.$nivelDificultad.'","'.$respCorrectas.'","'.$omisiones.'","'.$opcA.'","'.$opcB.'","'.$opcC.'",
-            "'.$opcD.'","'.$opcE.'","'.$opcF.'","'.$opcG.'","'.$opcH.'")
-        ';
-		if(!mysqli_query($cnx,$sql)){
-			echo "NO ".$cnt."<BR>";
-			$MALOS++;
-		}
-        $cnt++;		
+        $cnt++;	
+        if($cnt>1){
+            $idPregunta = $col[0];
+            $clave = $col[1];
+            $grado = $col[2];
+            $grupo = $col[3];
+            $instrumento = $col[4];
+            $cuadernillo = $col[5];
+            $componente = $col[6];
+            $competencia = $col[7];
+            $afirmacion = $col[8];
+            $evidencia = $col[9];
+            $nivelDificultad = $col[10];	
+            $respCorrectas = $col[11];	
+            $omisiones = $col[12];
+            $opcA = $col[13];
+            $opcB = $col[14];
+            $opcC = $col[15];
+            $opcD = 0;
+            $opcE = 0;
+            $opcF = 0;
+            $opcG = 0;
+            $opcH = 0;
+            echo '<td>'.$cnt.'</td>';
+            echo '<td>'.$idPregunta.'</td>';
+            echo '<td>'.$clave.'</td>';
+            echo '<td>'.$grado.'</td>';
+            echo '<td>'.$grupo.'</td>';
+            echo '<td>'.$instrumento.'</td>';
+            echo '<td>'.$cuadernillo.'</td>';
+            echo '<td>'.$componente.'</td>';
+            echo '<td>'.$afirmacion.'</td>';
+            echo '<td>'.$competencia.'</td>';
+            echo '<td>'.$evidencia.'</td>';
+            echo '<td>'.$nivelDificultad.'</td>';
+            echo '<td>'.$respCorrectas.'</td>';
+            echo '<td>'.$omisiones.'</td>';
+            echo '<td>'.$opcA.'</td>';
+            echo '<td>'.$opcB.'</td>';
+            echo '<td>'.$opcC.'</td>';
+            echo '<td>'.$opcD.'</td>';
+            echo '<td>'.$opcE.'</td>';
+            echo '<td>'.$opcF.'</td>';
+            echo '<td>'.$opcG.'</td>';
+            echo '<td>'.$opcH.'</td>';
+            echo '</tr>';			
+            $sql='
+                INSERT INTO analisisPreguntas(idPregunta,clave,grado,grupo,instrumento,cuadernillo,componente,afirmacion,competencia,evidencia,nivelDificultad,respCorrectas,omisiones,opcA,opcB,opcC,opcD,opcE,opcF,opcG,opcH) 
+                VALUES ("'.$idPregunta.'","'.$clave.'","'.$grado.'","'.$grupo.'","'.$instrumento.'",'.$cuadernillo.',"'.$componente.'","'.$afirmacion.'","'.$competencia.'",
+                "'.$evidencia.'","'.$nivelDificultad.'","'.$respCorrectas.'","'.$omisiones.'","'.$opcA.'","'.$opcB.'","'.$opcC.'",
+                "'.$opcD.'","'.$opcE.'","'.$opcF.'","'.$opcG.'","'.$opcH.'")
+            ';
+            if(!mysqli_query($cnx,$sql)){
+                echo "NO ".$cnt."<BR>";
+                $MALOS++;
+            }
+        }	
 	}
 	echo '</table>';
 	if($MALOS==0){
