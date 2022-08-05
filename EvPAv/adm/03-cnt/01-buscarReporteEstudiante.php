ea1520faff5b3a4b73dd218a432dfa82156d853d
bhagam19 <bhagam19@gmail.com>
2022-08-04 22:07:51 -0500

    Actualizacion estilo

--- EvPAv/adm/03-cnt/01-buscarReporteEstudiante.php
+++ EvPAv/adm/03-cnt/01-buscarReporteEstudiante.php
@@ -19,37 +19,39 @@
     }
     echo "
         <div class='encabezadoReporte'>
-            <div>Hola, ".ucwords(strtolower($estudiante))."</div>
-            <div>Presentaste ".$numPruebas." pruebas:</div>
+            <div class='reporteSaludo'>Hola, ".ucwords(strtolower($estudiante))."</div>
+            <div class='reporteCantidadPruebas'>Presentaste ".$numPruebas." pruebas:</div>
         </div>
+        <div class='reporteCuerpo'>
     ";    
     $consulta=$cnx->query('SELECT DISTINCT instrumento FROM analisisEstudiantes WHERE idEstudiante="'.$id.'"');    
     $cnt=0;
     while($fila=mysqli_fetch_array($consulta)){
-        $cnt++;
-        echo "<div class='cuerpoReporte'>";
-        echo "<div>".$cnt.") ".$fila['instrumento']."</div>";
+        $cnt++;        
+        echo "<div class='reporteInstrumento'>";
+        echo "<div class='reporteTituloInstrumento'>".$cnt.") ".$fila['instrumento']."</div>";
         $consultaTtlResp=$cnx->query('SELECT DISTINCT idPregunta FROM analisisEstudiantes WHERE idEstudiante="'.$id.'" 
                             AND instrumento="'.$fila['instrumento'].'" AND NOT idPregunta="Total resp"');
         $consultaRespCorrecta=$cnx->query('SELECT DISTINCT idPregunta, respEstudiante FROM analisisEstudiantes WHERE idEstudiante="'.$id.'" 
                             AND instrumento="'.$fila['instrumento'].'" AND NOT idPregunta="Total resp" AND clave = respEstudiante');
         $consultaRespIncorrecta=$cnx->query('SELECT DISTINCT idPregunta, clave, respEstudiante FROM analisisEstudiantes WHERE idEstudiante="'.$id.'" 
-                            AND instrumento="'.$fila['instrumento'].'" AND NOT idPregunta="Total resp" AND NOT clave = respEstudiante');
-                            
+                            AND instrumento="'.$fila['instrumento'].'" AND NOT idPregunta="Total resp" AND NOT clave = respEstudiante'); 
+        $consultaRespuestas=$cnx->query('SELECT DISTINCT idPregunta, clave, respEstudiante FROM analisisEstudiantes WHERE idEstudiante="'.$id.'" 
+                            AND instrumento="'.$fila['instrumento'].'" AND NOT idPregunta="Total resp"');                            
         $ttlResp=mysqli_num_rows($consultaTtlResp);
         $respCorrectas=mysqli_num_rows($consultaRespCorrecta);
         $respIncorrectas=mysqli_num_rows($consultaRespIncorrecta);
-        echo "<div>Respuestas correctas: ".$respCorrectas." de ".$ttlResp." (".($respCorrectas*(5/$ttlResp))." - ".(($respCorrectas/$ttlResp)*100)."%) </div>";
-        echo "<div>";
-        while($fila2=mysqli_fetch_array($consultaRespCorrecta)){
-            echo "<p>Pregunta ".$fila2['idPregunta']." = respuesta: ".$fila2['respEstudiante']."</p>";
-        }
-        echo"</div>";
-        echo "<div>Respuestas Incorrectas: ".$respIncorrectas."</div>";
-        echo "<div>";
-        while($fila3=mysqli_fetch_array($consultaRespIncorrecta)){
-            echo "<p>Pregunta ".$fila3['idPregunta']." = respuesta: ".$fila3['respEstudiante']." = clave: ".$fila3['clave']."</p>";
-        }
+        echo "<div class='reporteTituloRespuestasCorrectas'>Respuestas correctas: ".$respCorrectas." de ".$ttlResp.
+             " (".($respCorrectas*(5/$ttlResp))." - ".(($respCorrectas/$ttlResp)*100)."%) </div>";
+        echo "<div class='reporteTituloRespuestasIncorrectas'>Respuestas Incorrectas: ".$respIncorrectas."</div>";
+        echo "<div class='reporteResumenRespuestas'>";
+        while($fila4=mysqli_fetch_array($consultaRespuestas)){
+            if($fila4['respEstudiante']===$fila4['clave']){
+                echo "<p class='respuestaBuena'>Pregunta ".$fila4['idPregunta']." = respuesta: ".$fila4['respEstudiante']." = Bien.</p>";
+            }else{
+                echo "<p class='respuestaMala'>Pregunta ".$fila4['idPregunta']." = respuesta: ".$fila4['respEstudiante']." = Mal. Clave: ".$fila4['clave']."</p>";
+            }            
+        }        
         echo"</div>";
         echo "</div>";
     }       
